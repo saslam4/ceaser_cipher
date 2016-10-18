21cbfa81b8ec561b644d5f090f33d47763bf7763
Waqas A <syedwaqasaslam@gmail.com>
2016-10-18 18:35:55 -0500

    encoding

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -1,13 +1,21 @@
 def ceaser_cipher message = "", shift = 0
-    if !message.empty? then
-        bytes_array = Array.new()
+  if !message.empty? then
+      bytes_array = Array.new()
 
-        message.each_byte do |c|
-            bytes_array.push(c)
+      message.each_byte do |c|
+        shifted = c + shift
+        if (shifted > 90 && shifted < 97) || shifted > 122 then
+          shifted -= 26
+        elsif shifted < 65 || (shifted < 97 && shifted > 90) then
+          shifted += 26
         end
+        puts "shift by: #{shift} | regular: #{c} | shifted: #{shifted}"
+        bytes_array.push(shifted)
+      end
 
-        bytes_array
-    end
+      bytes_array.pack('c*')
+  end
 end
 
-puts ceaser_cipher "abc"
+
+puts ceaser_cipher "aA", 1
