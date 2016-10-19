9ebe9e2226d6d9ad7c0225aec6bb1e147e422377
Syed W. Aslam <syedwaqasaslam@gmail.com>
2016-10-18 22:58:43 -0500

    completed

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -1,21 +1,38 @@
+require 'sinatra'
+
+set :port, 8080
+
 def ceaser_cipher message = "", shift = 0
   if !message.empty? then
       bytes_array = Array.new()
 
       message.each_byte do |c|
-        shifted = c + shift
-        if (shifted > 90 && shifted < 97) || shifted > 122 then
-          shifted -= 26
-        elsif shifted < 65 || (shifted < 97 && shifted > 90) then
-          shifted += 26
-        end
-        puts "shift by: #{shift} | regular: #{c} | shifted: #{shifted}"
-        bytes_array.push(shifted)
+      	if (c > 64 && c < 91) || (c > 96 && c < 123) then
+
+	        shifted = c + shift
+	        if (shifted > 90 && shifted < 97) || shifted > 122 then
+	          shifted -= 26
+	        elsif shifted < 65 || (shifted < 97 && shifted > 90) then
+	          shifted += 26
+	        end
+	        bytes_array.push(shifted)
+	      else
+	      	bytes_array.push(c)
+	      end
+
       end
 
       bytes_array.pack('c*')
   end
 end
 
+get '/' do
+	@message = params['message']
+	@shift = params[:shift].to_i
 
-puts ceaser_cipher "aA", 1
+	if !@message.nil? then
+		@encodedMessage = ceaser_cipher @message, @shift
+	end
+
+	erb :index
+end
